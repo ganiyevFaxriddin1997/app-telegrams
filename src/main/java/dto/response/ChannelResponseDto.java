0b61bd7288140457e12ec06e694a91b8e21f9f39
acer <sardorbeksaydullayev731@gmail.com>
2023-03-11 13:48:57 -0700

    mana soat 1:23

--- src/main/java/dto/response/ChannelResponseDto.java
+++ src/main/java/dto/response/ChannelResponseDto.java
@@ -5,13 +5,17 @@ import models.post.Post;
 
 import java.util.List;
 import java.util.UUID;
-@AllArgsConstructor@NoArgsConstructor@Getter@Setter@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ChannelResponseDto {
 
     private String photo;
     private String bio;
     private String link;
-    private List<Post> posts;
+    private List<UUID> posts;
     private String name;
     private List<UUID> usersId;
 }
